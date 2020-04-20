009428267a89174a02507af02c676f53b29774ea
BlackskillsNL <31846722+BlackskillsNL@users.noreply.github.com>
2020-04-20 11:33:44 +0200

    My portfolio to repo

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 const timeline = new TimelineMax();
 
 timeline
-  .from('main', 0.5, { scale: 0, opacity: 0.5 }, '=+.25')
+  .from('main', 0.5, { scale: 0, opacity: 0.5 , rotation: 360}, '=+.25')
   .from('main > h1', 0.5, { y: -200, opacity: 0 }, '=+.5')
   .from('p', 1, { x: -200, opacity: 0 }, '=-.5')
   .from('section', 0.5, { y: 500, opacity: 0 }, '=-1')
